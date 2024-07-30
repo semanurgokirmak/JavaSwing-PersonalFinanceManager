06d3ff550ad7835209d3a5daddea7f4480ef2831
Semanur Gökırmak <154887690+semanurgokirmak@users.noreply.github.com>
2024-07-31 01:23:54 +0300

    Add files via upload

---
+++ java-borchesaplama/borchesaplama/src/borchesaplama/Borchesaplama.java
@@ -0,0 +1,17 @@
+
+package borchesaplama;
+
+/**
+ *
+ * @author Semanur Gökırmak
+ */
+public class Borchesaplama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
